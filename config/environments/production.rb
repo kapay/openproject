30c83921d74adbf441ed549ea561ee761b17276c
kapay <kapay@users.noreply.github.com>
2015-03-25 16:25:12 -0700

    Set config.assets.compile to true.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ OpenProject::Application.configure do
   config.assets.js_compressor = Uglifier.new(mangle: false) if defined? Uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
